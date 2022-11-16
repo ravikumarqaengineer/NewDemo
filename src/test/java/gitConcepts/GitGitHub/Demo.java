748ee1f6fbc5ce1448c9b250185dfa663fc546c7
ravikumar <ravikumar@testing.com>
2022-11-16 22:43:06 +0530

    from Y

--- src/test/java/gitConcepts/GitGitHub/Demo.java
+++
@@ -1,8 +0,0 @@
-package gitConcepts.GitGitHub;
-
-public class Demo {
-	public static void main(String[] args) {
-
-		 System.out.println("hi hello" ); 
-	}
-}
---
+++ src/test/java/gitConcepts/GitGitHub/DemoY.java
@@ -0,0 +1,12 @@
+package gitConcepts.GitGitHub;
+
+public class DemoY {
+	public static void main(String[] args) {
+
+		 System.out.println("hi hello" ); 
+		 System.out.println("hi hello" ); 
+ 		 System.out.println("hi hello" ); 
+ 		 System.out.println("hi hello" );  
+ 		 System.out.println("hi hello" ); 
+	}
+}
